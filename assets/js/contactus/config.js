3815b86c19693ed4d43c4fcea724138c0fd03ee6
Lal Mohan <lal@amt.in>
2021-09-06 19:25:00 +0530

    POD-1708 contact us mail fix

---
+++ assets/js/contactus/config.js
@@ -0,0 +1,9 @@
+var mailConfig = {
+  from: 'noreply-alerts@verteil.com',
+  to: 'contact@verteil.com',
+  subject: 'Business Enquiry via verteil.com',
+  greetingText: 'Hello Team,',
+  greetingDesc: 'We have a new business enquiry on verteil.com and here are the details:',
+  regardsText: 'Business Enquiry Automation.',
+  successText:'Thank you for reaching out to Verteil, we will respond back shortly'
+}
